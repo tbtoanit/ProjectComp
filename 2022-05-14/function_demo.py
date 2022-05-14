20e44f12112ab2dd4b78db08f0a18de51a4cec2c
Trần Bảo Toàn <49049899+tbtoanit@users.noreply.github.com>
2022-05-14 19:33:13 +0700

    Add files via upload

---
+++ 2022-05-14/function_demo.py
@@ -0,0 +1,23 @@
+from fb_func import xac_thuc_email, luu_thong_tin_nguoi_dung
+
+
+def tinh_toan(a, b, c):  # a,b argument, parameter
+    if c == '+':
+        ket_qua = a + b
+    elif c == '-':
+        ket_qua = a -b
+    elif c == '*':
+        ket_qua = a*b
+    elif c == '/':
+        ket_qua = a/b
+
+    return ket_qua
+
+
+if __name__ == "__main__": #đoạn code này chỉ được thực thi khi developer run trực tiếp từ file này
+    # tạo tài khoản facebook
+    # 1: Gửi email xác thực vào email đã đăng ký trên tk facebook
+    # 2: lưu thông tin các bạn vào cở sở dữ
+    print("Cod chay trong file function demo")
+    xac_thuc_email()
+    luu_thong_tin_nguoi_dung()
